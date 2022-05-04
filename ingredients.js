66faf4fcf6f809a9d8c46f52af8829dda75a8269
Ruanruanx <zxin7@uwo.ca>
2022-05-04 02:39:56 +0000

    while

---
+++ ingredients.js
@@ -0,0 +1,7 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
