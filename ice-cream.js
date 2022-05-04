0e29a35a09144153d5fb15f167d541d136bd97e8
Ruanruanx <zxin7@uwo.ca>
2022-05-04 02:30:23 +0000

    looparray

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+let newarr = iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours)
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length)
